ca73fe355fc066cf12e09c7d8735d2365d3886dd
Lam Nguyen Huu Phuc <86996855+LamNguyen2608@users.noreply.github.com>
2021-10-11 08:58:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
 const http = require("http");
 const hostname = "localhost";
-const port = process.env.PORT || 3000;
 const fs = require("fs");
-const server = http.createServer((req,res) => {
+const path = require("path");
+const express = require("Express");
+const app = express();
+const router = express.Router();
+const { append } = require("vary");
+const port = process.env.PORT || 3000;
+
+//image source
+app.use('/images', express.static(__dirname + '/images'));
+
+
+router.get('/', (req,res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    switch (req.url){
-        case "/home":
-            res.writeHead(200);
-            var data = fs.readFileSync('./Index.html');
-            res.end(data.toString());
-            break;
-        case "/about":
-            res.writeHead(200);
-            var data1 = fs.readFileSync('./About.html');
-            res.end(data1.toString());
-            break;
-        case "/cart":
-            res.writeHead(200);
-            var data2 = fs.readFileSync('./Cart.html');
-            res.end(data2.toString());
-            break;
-        default:
-            res.writeHead(200);
-            res.end("<h1>Easy on me</h1>")
-            break;
-    }
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Index.html");
+    res.end(data.toString());
+});
+router.get('/cart', (req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Cart.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/Cart.html');
+});
+router.get('/product', (req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Product.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/Product.html');
+});
+router.get('/signup', (req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./Signup.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/Signup.html');
+});
+router.get('/login', (req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./User_login.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/User_login.html');
+});
+router.get('/about', (req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.writeHead(200);
+    var data = fs.readFileSync("./About.html");
+    res.end(data.toString());
+    //res.sendFile(__dirname + '/About.html');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-});
+app.use("/", router);
+app.listen(port, () => console.log('Listening'));
